ab51f695f8b90e2961d2d45d93f0f68fca70cfa1
Matias Paez molina <paezmolina@gmail.com>
2023-11-29 16:46:59 -0300

    Commit.

--- pages/api/eth/list-groups.js
+++ pages/api/eth/list-groups.js
@@ -1,7 +1,3 @@
-import factory from "../../../ethereum/game";
 export default async (req, res) => {
-  const groups = await factory.methods.getDeployedGroups().call();
-
-  res.statusCode = 200;
-  res.json(groups);
+  res.json("");
 };
